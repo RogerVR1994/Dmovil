758f276965ab9fc3ad67913c5623c3986c231811
Rogelio Ventura <rogeliovr94@gmail.com>
2018-05-04 02:37:32 -0500

    prueba

--- PHP/sign_up.php
+++ PHP/sign_up.php
@@ -3,12 +3,9 @@
   /*
   Código para registrar nuevo usuario y comprobar existencia
   */
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
-  require_once "../../../connect.php"; //Correr código de incio de sesión en MySQL
+  include "../../../connect.php"; //Correr código de incio de sesión en MySQL
 
-
+  $link=db_Connection(); //Crear conexión MySQL
 
   $command = escapeshellcmd("python3 ../Python/user.py"); //Correr código en Python para crear ID único de usuario
   $user = shell_exec($command); //Obtener respuesta de código de Python
@@ -16,31 +13,19 @@
   $apellido=$_POST["apellido"];
   $email=$_POST["email"];
   $password=$_POST["password"];
-  $cuenta = $_POST['cuenta'];
-  $banco = $_POST['banco'];
-  foreach($_POST as $post){
-    echo $post;
-    echo '<br>';
-  }
-
   $key="yumyum"; //Llave para encriptar contraseñas
 
+  $check_user_query = $link->query("SELECT correo as mail FROM Usuarios WHERE correo ='".$email."' ");//Query para comprobar existencia de usuario
 
 
   if (mysqli_num_rows($check_user_query) == 0) { //Si el correo no existe en la base de datos se agregan todos los datos correspondientes a la tabla
-
-    $sql = "INSERT INTO usuarios VALUES (NULL, NULL, 'Rogelio', 'Ventura', '234234', 'banco', 'banco@gmail.com', 'afasdfasf')";
-    $query = $pdo->prepare($sql);
-    $result = $query->execute([
-      'nombre'=> $nombre,
-      'apellido' => $apellido,
-      'cuenta' => $cuenta,
-      'banco' => $banco, 
-      'email' => $email,
-      'password' => $password
-    ]);
-    echo "hola";
-    header("Location: ../dashboard.php"); //redirección a página de inicio
+    echo "vacio";
+    $stmt = $link->prepare("INSERT INTO Usuarios VALUES (?, ?, ?, ?, AES_ENCRYPT(?, ?))zz");
+    $stmt->bind_param("ssssss", $user, $nombre, $apellido, $email, $password, $key);
+    $stmt->execute();
+    $stmt->close();//Cerrar conexiones de MySQL
+    $link->close();
+    header("Location: ../dashboard.html"); //redirección a página de inicio
   } 
 
   else { //Si el correo ya existe en la tabla de datos se envía mensaje de error para ingresar otro correo
