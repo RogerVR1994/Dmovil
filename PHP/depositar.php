45a1e3ed52500b1770b051701204843a09d4cec9
Rogelio Ventura <rogeliovr94@gmail.com>
2018-05-04 08:26:14 -0500

    DIsplay saldo

--- PHP/depositar.php
+++ PHP/depositar.php
@@ -4,4 +4,5 @@ include "../../../connect.php";
   $link= db_Connection();
   $monto = $_POST['monto'];
   $result= $link->query("UPDATE Usuarios set saldo = saldo +".$monto." where nombre = 'Rogelio'");
+  header('Location: ../dashboard.php');
 ?>
