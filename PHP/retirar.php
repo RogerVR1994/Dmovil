210eb0a2603c1b83c1ae918ada0cce3f3052dcf4
Rogelio Ventura <rogeliovr94@gmail.com>
2018-05-04 08:22:22 -0500

    DIsplay saldo

---
+++ PHP/retirar.php
@@ -0,0 +1,7 @@
+<?php
+include "../../../connect.php";
+  
+  $link= db_Connection();
+  $monto = $_POST['monto'];
+  $result= $link->query("UPDATE Usuarios set saldo = saldo -".$monto." where nombre = 'Rogelio'");
+?>
